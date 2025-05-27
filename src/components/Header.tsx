237830b9c3ed6a80a54b0baa78e2d9d2e0a6a6c8
smartgamesbr <110876722+smartgamesbr@users.noreply.github.com>
2025-05-27 08:03:09 -0300

    Set up modern layout with required dependencies

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { APP_NAME } from '../constants';
 import { User } from '../types';
 import { Bars3Icon } from '@heroicons/react/24/outline';

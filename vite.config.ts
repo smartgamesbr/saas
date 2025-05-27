185b433f75f986b1d643ab66178646f9ad53df2a
smartgamesbr <110876722+smartgamesbr@users.noreply.github.com>
2025-05-27 07:20:01 -0300

    Configure Gemini API Key

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'import.meta.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'import.meta.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
